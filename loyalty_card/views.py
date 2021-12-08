dc773304d62ab551c57f60327d58c5216d283c52
KennethWu69 <71262989+KennethWu69@users.noreply.github.com>
2021-12-08 15:48:46 +0800

    update

--- loyalty_card/views.py
+++ loyalty_card/views.py
@@ -104,7 +104,7 @@ def callback(request):
                     stage = user.stage
                 if re.match(stage, "簡歷工作坊"):
                     message.append(TextSendMessage(text='//登記簡歷工作坊集點卡中\n'))
-                    temp = '學號$s'%(event.message.text)
+                    temp = '學號%s'%(event.message.text)
                     message.append(TextSendMessage(text=temp))
                     if Sheet.objects.filter(student_id = event.message.text).exists() == False:
                         message.append(TextSendMessage(text='//您尚未報名或是輸入非學號字元\n'))
